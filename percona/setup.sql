e78b342bd9921a559910e4442ad0782e35dd897e
Gabriel Mateescu <gabriel.mateescu@gmail.com>
2021-08-22 04:12:17 +0200

    Volume paths

---
+++ percona/setup.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `westwing_sample` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `value` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
